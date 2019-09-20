73209e8a5c530ed1fd15284ace09cc2fd2d4365a
SmackSmash <danmoffat@gmail.com>
2019-09-20 14:42:30 +0100

    Added basic resux saga setup to store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,16 @@
 import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const enhancers = composeEnhancers(applyMiddleware(thunk));
+const enhancers = composeEnhancers(applyMiddleware(sagaMiddleware));
 
 const store = createStore(rootReducer, {}, enhancers);
+
+// sagaMiddleware.use()
 
 export default store;
