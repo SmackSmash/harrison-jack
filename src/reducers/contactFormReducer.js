3e7399e5032a5865242399e28cc3c5f86359bd13
SmackSmash <danmoffat@gmail.com>
2019-09-20 15:57:50 +0100

    Added robust error handling for async form submission

--- src/reducers/contactFormReducer.js
+++ src/reducers/contactFormReducer.js
@@ -1,4 +1,8 @@
-import { SUBMIT_CONTACT_FORM_SUCCESS, SUBMIT_CONTACT_FORM_ERROR } from '../actions/types';
+import {
+  SUBMIT_CONTACT_FORM,
+  SUBMIT_CONTACT_FORM_SUCCESS,
+  SUBMIT_CONTACT_FORM_ERROR
+} from '../actions/types';
 
 const INITIAL_STATE = {
   sent: false,
@@ -8,6 +12,12 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case SUBMIT_CONTACT_FORM:
+      return { sent: false, sending: true, error: null };
+    case SUBMIT_CONTACT_FORM_ERROR:
+      return { sent: false, sending: false, error: action.payload };
+    case SUBMIT_CONTACT_FORM_SUCCESS:
+      return { sent: true, sending: false, error: null };
     default:
       return state;
   }
--- src/sagas/contactFormSaga.js
+++ src/sagas/contactFormSaga.js
@@ -19,9 +19,19 @@ function* submitContactFormAsync(action) {
   formData.set('email', email);
   formData.set('phone', phone);
   formData.set('message', message);
-  const response = yield axios.post('http://build.harrisonjack.co.uk/api/sendMail.php', formData);
-  console.log(response);
-  response.status === 200
-    ? yield put({ type: SUBMIT_CONTACT_FORM_SUCCESS })
-    : yield put({ type: SUBMIT_CONTACT_FORM_ERROR });
+  try {
+    const response = yield axios.post('http://build.harrisonjack.co.uk/api/sendMail.php', formData);
+    response.status === 200
+      ? yield put({ type: SUBMIT_CONTACT_FORM_SUCCESS })
+      : yield put({
+          type: SUBMIT_CONTACT_FORM_ERROR,
+          payload: `Error ${response.status}: Your message could not be sent`
+        });
+  } catch (error) {
+    console.log(error);
+    yield put({
+      type: SUBMIT_CONTACT_FORM_ERROR,
+      payload: error.message
+    });
+  }
 }
