aa3fca2b16c92f1e9cce9a7a0a5ef738aa671746
SmackSmash <danmoffat@gmail.com>
2019-09-23 00:31:24 +0100

    Updated footer sttles and link hover effect

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import './footer.styles.scss';
 
 const Footer = props => {
-  const currentYear = new Date().getFullYear();
   return (
     <footer>
       <div className="footer">
-        <p className="footer__copyright">Copyright {currentYear} HarrisonJack</p>
+        <p className="footer__copyright">Copyright {new Date().getFullYear()} HarrisonJack</p>
       </div>
     </footer>
   );
