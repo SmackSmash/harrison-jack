ef87f6288bdf40b3256447ae84624557d68c81a7
Dan Moffat <danmoffat@gmail.com>
2019-10-01 20:57:53 +0100

    Finish for the night

--- src/components/mobile-logo/mobile-logo.component.jsx
+++ src/components/mobile-logo/mobile-logo.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/images/logo.svg';
 import './mobile-logo.styles.scss';
 
 const MobileLogo = props => {
   return (
     <div className="mobile-logo">
-      <Logo />
+      <Link exact to="/">
+        <Logo />
+      </Link>
     </div>
   );
 };
